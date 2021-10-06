23b4dfd57cf64152d8f9aa75d79abb8499ccad98
Hiep Nguyen <nxhiep.1996@gmail.com>
2021-10-06 19:58:55 +0700

    minor change

--- content-script.js
+++ content-script.js
@@ -1,75 +1,209 @@
+const KEY = 'clone_data_hello_world';
 
-console.log("hello world")
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-if (window.location.href.indexOf('accuplacerpracticetest.com') > -1) {
-    const questionId = ".mtq_question_text h3";
-    const hintId = ".mtq_question_text div";
-    const imageId = "";
-    const correctAnswerId = ".mtq_marker.mtq_correct_marker";
-    const inCorrectAnswerId = ".mtq_marker.mtq_wrong_marker";
-    const answerContentId = ".mtq_answer_text";
-    const explanationAnswerId = ".mtq_explanation-text";
+function getTopicName() {
+    let prefix = '/practice-test';
+    let pathname = window.location.pathname;
+    let offset = pathname.indexOf(prefix) + prefix.length;
+    let limit = pathname.indexOf('/', offset);
+    return pathname.substring(offset, limit);
+}
 
-    const getContent = (key, element) => {
-        let elem = element.querySelector(key);
-        if (elem && elem.innerHTML && elem.innerHTML.indexOf("MathJax") > -1) {
-            return elem.innerHTML;
-        }
-        return elem?.innerText ?? "";
-    }
+function getQuestionId() {
+    let prefix = '/pages/';
+    let pathname = window.location.pathname;
+    let offset = pathname.indexOf(prefix) + prefix.length;
+    return pathname.substring(offset, pathname.length);
+}
 
-    const getQuestionData = (element) => {
-        let question = getContent(questionId, element);
-        let hint = getContent(hintId, element);
-        let explanationAnswer = getContent(explanationAnswerId, element);
+function getCookie(key) {
+    return localStorage.getItem(key);
+    // var name = key + "=";
+    // var decodedCookie = decodeURIComponent(document.cookie);
+    // var ca = decodedCookie.split(";");
+    // for (var i = 0; i < ca.length; i++) {
+    //     var c = ca[i];
+    //     while (c.charAt(0) == " ") {
+    //         c = c.substring(1);
+    //     }
+    //     if (c.indexOf(name) == 0) {
+    //         return c.substring(name.length, c.length);
+    //     }
+    // }
+    // return "";
+}
 
-        let correctAnswersElement = element.querySelectorAll(correctAnswerId);
-        let inCorrectAnswersElement = element.querySelectorAll(inCorrectAnswerId);
+function setCookie(key, value) {
+    // var d = new Date();
+    // d.setTime(d.getTime() + 10 * 24 * 60 * 60 * 1000);
+    // var expires = "expires=" + d.toUTCString();
+    // document.cookie = key + "=" + value + ";" + expires + ";path=/";
+    localStorage.setItem(key, value);
+}
 
-        let correctAnswers = [];
-        correctAnswersElement?.forEach((element) => {
-            let text = getContent(answerContentId, element.parentElement.parentElement);
-            if (text && correctAnswers.indexOf(text) == -1) {
-                correctAnswers.push(text);
-            }
-        });
-        let inCorrectAnswers = [];
-        inCorrectAnswersElement?.forEach((element) => {
-            let text = getContent(answerContentId, element.parentElement.parentElement);
-            if (text && inCorrectAnswers.indexOf(text) == -1) {
-                inCorrectAnswers.push(text);
-            }
-        });
+var stop = typeof getCookie(KEY) === 'undefined' || getCookie(KEY) == 'true';
 
-        return {
-            question,
-            hint,
-            correctAnswers,
-            inCorrectAnswers,
-            explanationAnswer
-        }
-    }
+function downloadJson(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
 
-    setTimeout(() => {
-
-        let questionsElement = document.querySelectorAll(".mtq_question");
-        let questions = [];
-        questionsElement.forEach((element) => {
-            let questionData = getQuestionData(element);
-            questions.push(questionData);
-        });
+if (window.location.href.indexOf('uniontestprep.com') > -1) {
+    console.log(window.location.pathname)
+    if (window.location.pathname.indexOf('profile') > -1) {
+        let key = "all-question";
+        let questionsStr = getCookie(key);
+        let questions = JSON.parse(questionsStr || "[]") || [];
+        console.log("OK", questions)
         if (questions.length > 0) {
-            let name = document.querySelector('.entry-title[itemprop="headline"]').innerText;
-            downloadJson(JSON.stringify(questions), name + '.json', 'text/plain');
+            let topicName = questions[0].topicName + "_" + new Date().getTime();
+            downloadJson(JSON.stringify(questions), topicName + '.json', 'text/plain');
         }
-    }, 1000);
+    } else {
+        if (!stop) {
+            selectQuestion();
+        }
+    }
+    let buttonStart = document.createElement('button');
+    buttonStart.innerHTML = 'Start';
+    buttonStart.onclick = () => {
+        setCookie(KEY, 'false');
+        selectQuestion();
+    }
+    buttonStart.setAttribute('style', `
+        position: fixed;
+        top: 10px;
+        left: 10px;
+        padding: 10px 30px;
+        border-radius: 10px;
+        background: green;
+        color: white;
+    `);
+    let buttonStop = document.createElement('button');
+    buttonStop.innerHTML = 'Stop';
+    buttonStop.onclick = () => {
+        setCookie(KEY, 'true');
+    }
+    buttonStop.setAttribute('style', `
+        position: fixed;
+        top: 10px;
+        left: 120px;
+        padding: 10px 30px;
+        border-radius: 10px;
+        background: red;
+        color: white;
+    `);
+    document.body.appendChild(buttonStart);
+    document.body.appendChild(buttonStop);
+}
 
+async function selectQuestion() {
+    let formElement = document.querySelector('#new_response');
+    if (!formElement) {
+        window.alert('OK');
+        return;
+    }
+    await sleep(500);
+    let explanationElement = formElement.querySelectorAll('fieldset');
+    if (!explanationElement) {
+        window.alert('OK');
+        return;
+    }
+    if (explanationElement.length > 1) {
+        explanationElement = explanationElement[explanationElement.length - 1];
+    } else {
+        explanationElement = null;
+    }
+    // let check = Object.values(explanationElement?.querySelectorAll('#test-question') || {}).find((a) => a.innerText == 'Explanation:');
+    console.log("formElement", formElement)
+    let check = !!formElement;
+    if (check) {
+        let newQuestion = await getQuestion(formElement, explanationElement);
+        let buttonNextQuestion = document.querySelector('#btn-next-question');
+        // TODO
+        // fetch("http://localhost:4000/save-data", {
+        //     method: "POST",
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(question)
+        // }).then(res => {
+        //     console.log("Request complete! response:", res);
+        //     if (res.status == 200) {
+        //         setTimeout(() => {
+        //             buttonNextQuestion && buttonNextQuestion.click();
+        //         }, 300);
+        //     } else {
+        //         window.alert('xxxxxxxxxxxxxxxxxxxxxxx')
+        //     }
+        // });
 
-    function downloadJson(content, fileName, contentType) {
-        var a = document.createElement("a");
-        var file = new Blob([content], { type: contentType });
-        a.href = URL.createObjectURL(file);
-        a.download = fileName;
-        a.click();
+        setTimeout(() => {
+            let key = "all-question";
+            let questionsStr = getCookie(key);
+            let questions = JSON.parse(questionsStr || "[]") || [];
+            let existed = questions.findIndex((question) => question.id == newQuestion.id) > -1;
+            if (!existed) {
+                questions.push(newQuestion);
+                setCookie(key, JSON.stringify(questions));
+            }
+            console.log("total question: ", existed, questions.length, newQuestion);
+            setTimeout(() => {
+                buttonNextQuestion && buttonNextQuestion.click();
+            }, 1000);
+        }, 1000);
+        return newQuestion;
+    } else {
+        let input = formElement.querySelector('#answer-options input');
+        input && input.click();
+        let showAnswerElement = formElement.querySelector('a[href="?show_explanation=true"]')
+        showAnswerElement && showAnswerElement.click();
+    }
+    return null;
+}
+
+async function getQuestion(formElement, explanationElement) {
+    let button = formElement.querySelector('#question-aid button');
+    let modalId = '';
+    if (button) {
+        modalId = button.getAttribute('data-target');
+        button.click();
+    }
+    await sleep(1000);
+    let imageElement = modalId ? document.querySelector(modalId + ' img') : null;
+    let image = imageElement && imageElement.getAttribute('src') || '';
+    let paragraphElement = modalId ? document.querySelector(modalId + ' .markdown-content') : null;
+    let paragraph = paragraphElement && paragraphElement.innerHTML || '';
+    let questionElement = formElement.querySelector('#question-text');
+    let answersElement = formElement.querySelector('#answer-options');
+    let text = questionElement.querySelector('.markdown-content').innerHTML;
+    let answers = Object.values(answersElement.querySelectorAll('.markdown-content')).map(e => e.innerHTML.replace('\n', ''));
+    let explanationContentElement = explanationElement.querySelectorAll('.markdown-content');
+    let explanation = '', correctAnswer = '';
+    if (explanationContentElement.length == 2) {
+        explanation = explanationContentElement[explanationContentElement.length - 1].innerHTML;
+        correctAnswer = explanationContentElement[explanationContentElement.length - 2].innerHTML;
+    } else if (explanationContentElement.length == 1) {
+        explanation = explanationContentElement[0].innerHTML;
+        let x = answersElement.querySelector('input[checked="checked"]');
+        if (x) {
+            correctAnswer = x.parentElement.querySelector('.markdown-content').innerHTML
+        }
+    }
+    return {
+        id: getQuestionId(),
+        topicName: getTopicName(),
+        text: text.replace('\n', ''),
+        answer: answers,
+        explanation: explanation.replace('\n', ''),
+        correctAnswer: correctAnswer.replace('\n', ''),
+        image: image.replace('\n', ''),
+        paragraph: paragraph.replace('\n', ''),
     }
 }
---
+++ content-script_accuplacerpracticetest.js
@@ -0,0 +1,77 @@
+
+console.log("hello world")
+
+if (window.location.href.indexOf('accuplacerpracticetest.com') > -1) {
+    const questionId = ".mtq_question_text h3";
+    const hintId = ".mtq_question_text div";
+    const imageId = ".mtq_question_text img";
+    const correctAnswerId = ".mtq_marker.mtq_correct_marker";
+    const inCorrectAnswerId = ".mtq_marker.mtq_wrong_marker";
+    const answerContentId = ".mtq_answer_text";
+    const explanationAnswerId = ".mtq_explanation-text";
+
+    const getContent = (key, element) => {
+        let elem = element.querySelector(key);
+        if (elem && elem.innerHTML && elem.innerHTML.indexOf("MathJax") > -1) {
+            return elem.innerHTML;
+        }
+        return elem?.innerText ?? "";
+    }
+
+    const getQuestionData = (element) => {
+        let question = getContent(questionId, element);
+        let hint = getContent(hintId, element);
+        let image = getContent(imageId, element);
+        let explanationAnswer = getContent(explanationAnswerId, element);
+
+        let correctAnswersElement = element.querySelectorAll(correctAnswerId);
+        let inCorrectAnswersElement = element.querySelectorAll(inCorrectAnswerId);
+
+        let correctAnswers = [];
+        correctAnswersElement?.forEach((element) => {
+            let text = getContent(answerContentId, element.parentElement.parentElement);
+            if (text && correctAnswers.indexOf(text) == -1) {
+                correctAnswers.push(text);
+            }
+        });
+        let inCorrectAnswers = [];
+        inCorrectAnswersElement?.forEach((element) => {
+            let text = getContent(answerContentId, element.parentElement.parentElement);
+            if (text && inCorrectAnswers.indexOf(text) == -1) {
+                inCorrectAnswers.push(text);
+            }
+        });
+
+        return {
+            question,
+            hint,
+            image,
+            correctAnswers,
+            inCorrectAnswers,
+            explanationAnswer
+        }
+    }
+
+    setTimeout(() => {
+
+        let questionsElement = document.querySelectorAll(".mtq_question");
+        let questions = [];
+        questionsElement.forEach((element) => {
+            let questionData = getQuestionData(element);
+            questions.push(questionData);
+        });
+        if (questions.length > 0) {
+            let name = document.querySelector('.entry-title[itemprop="headline"]').innerText;
+            downloadJson(JSON.stringify(questions), name + '.json', 'text/plain');
+        }
+    }, 1000);
+
+
+    function downloadJson(content, fileName, contentType) {
+        var a = document.createElement("a");
+        var file = new Blob([content], { type: contentType });
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
+    }
+}
